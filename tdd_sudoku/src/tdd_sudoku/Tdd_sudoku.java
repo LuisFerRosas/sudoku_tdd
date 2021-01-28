be7bc372c356bc23dfa8bc5cf4d08229a2c02de0
Fernando <luis3658duran@gmail.com>
2021-01-28 18:57:28 -0400

    primer test fallo

--- tdd_sudoku/src/tdd_sudoku/Tdd_sudoku.java
+++ tdd_sudoku/src/tdd_sudoku/Tdd_sudoku.java
@@ -5,17 +5,36 @@
  */
 package tdd_sudoku;
 
+import com.sun.org.apache.xml.internal.security.transforms.TransformationException;
+
 /**
  *
  * @author Fernando
  */
 public class Tdd_sudoku {
-
+    public static int [][] tabla={{5,1,6,9,8,4,3,7,2},
+                     {3,7,8,6,5,2,9,4,1},
+                     {9,2,4,3,7,1,5,8,6},
+                     {2,6,7,5,9,8,4,1,3},
+                     {1,3,5,4,2,7,6,9,8},
+                     {4,8,9,1,6,3,7,2,5},
+                     {8,9,3,7,1,6,2,5,4},
+                     {7,4,1,2,3,5,8,6,9},
+                     {6,5,2,8,4,9,1,3,7}};
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
+    
+    public static int [] tamanio_tabla(){
+        
+        return null;
+    }
+   
+    
+    
     
 }
